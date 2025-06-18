a8c8dd511ad862530facb39526cb1ba46d346796
Gajendra-gif <sahuugajendra@gmail.com>
2025-06-19 03:53:46 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'us-east-1_8J6AydIXf', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '7mfui9sp66db2ib6liej7s38b9', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: '' // e.g. us-east-2
     },
     api: {
